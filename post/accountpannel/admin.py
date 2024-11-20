21c120cbd69f4080d04326347bdcc93de0865852
rahuljoshi1729 <rahul22100@akgec.ac.in>
2024-11-20 16:56:53 +0530

    added import/export button

--- post/accountpannel/admin.py
+++ post/accountpannel/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
+from django.apps import apps
 from .models import User, customer
+from .admin_utils import *
 
-admin.site.register(User)
-admin.site.register(customer)
+
+app_models = apps.get_app_config('accountpannel').get_models()
+
+for model in  app_models:
+    class  ModelResource(GenericResource):
+        class Meta:
+            model = model
+            
+    class ModelAdmin(ImportExportAdmin):
+        resource_class = ModelResource
+    
+    try:
+        admin.site.register(model, ModelAdmin)
+    except admin.sites.AlreadyRegistered:
+        pass               
