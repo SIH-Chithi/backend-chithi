946a110118e0ab8554e50e9b5aaa5639f7e44932
rahuljoshi1729 <rahul22100@akgec.ac.in>
2024-12-02 12:27:03 +0530

    added routes of nsh

--- post/employee/urls.py
+++ post/employee/urls.py
@@ -12,13 +12,19 @@ urlpatterns = [
     path('create_container/', create_container.as_view(), name='create_container'),
     path('get_employee_details/', employee_details.as_view(), name='get_employee_details'),
     path('get_containers/', get_containers.as_view(), name='get_containers'),
+    path('get_container_details/', get_details_container.as_view(), name='get_container_details'),
     path('relate_consignment_container/', relate_consignment_container.as_view(), name='relate_consignment_container'),
-    path('checkout_to_hpo/', checkout_to_hpo.as_view(), name='checkout_to_hpo'),
+    path('checkout/', checkout.as_view(), name='checkout_to_hpo'),
     path('get_checkout_container/', get_container_checked_out.as_view(), name='get_checkout_container'),
-    path('checkin_to_spo/', checkin_to_spo.as_view(), name='checkin_to_spo'),
+    path('checkin/', checkin.as_view(), name='checkin_to_spo'),
     path('get_checkin_container/', get_container_checked_in.as_view(), name='get_checkin_container'),
     path('generate_container_qr/',generate_qr_container.as_view(),name='generate_container_qr'),
     path('book_consignment_hpo/', book_consignment_hpo.as_view(), name='book_consignment_hpo'),
+    path('secondary_sort/', secondary_sorting.as_view(), name='secondary_sorting'),
+    
+    #NSH
+    path('preliminary_sort/', preliminary_sorting.as_view(), name='preliminary_sorting'),
+    path('checkin_to_nsh/', checkin_NSH.as_view(), name='checkin_to_nsh'),
 
 ]
 
